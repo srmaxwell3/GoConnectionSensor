8f000594261c9b55487a1da8547d2395a270da2b
Sid Maxwell <srmaxwell@gmail.com>
2018-11-27 22:07:38 +0000

    Minor tweaks.

--- s.cpp
+++ s.cpp
@@ -1,18 +1,16 @@
 #include <bitset>
-
+using std::bitset;
 template<size_t S> struct square_bitset: public bitset<S * S> {
   square_bitset() {
     bitset<S * S>::reset();
   }
-
   static size_t at(int row, int col) {
-    return (size_t(row) * S + size_t(col);
+    return (size_t(row) * S + size_t(col));
   }
-
   bool operator()(int row, int col) const {
       return (*this)[at(row, col)];
   }
-  BitSet::reference operator()(int row, int col) {
+  bitset<S * S>::reference operator()(int row, int col) {
     return (*this)[at(row, col)];
   }
 };
